db808ddbc3e894cf6e7732d36241e1468990b0f3
VoldemarM <aniairina@gmail.com>
2023-04-08 15:34:48 +0300

    formatting code

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -4,33 +4,43 @@
 //  5-> Пятница
 Console.WriteLine("Введите число одного до девяти:");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number== 1) {
+if (number == 1)
+{
     Console.WriteLine("Понедельник");
 }
-if (number== 2) {
+if (number == 2)
+{
     Console.WriteLine("Вторник");
 }
-if (number== 4) {
+if (number == 4)
+{
     Console.WriteLine("Четверг");
 }
-if (number== 3) {
+if (number == 3)
+{
     Console.WriteLine("Среда");
 }
-if (number== 5) {
+if (number == 5)
+{
     Console.WriteLine("Пятница");
 }
-if (number== 6) {
+if (number == 6)
+{
     Console.WriteLine("Шестица");
 }
-if (number== 7) {
+if (number == 7)
+{
     Console.WriteLine("Седьмица");
 }
-if (number== 8) {
+if (number == 8)
+{
     Console.WriteLine("Осьмица");
 }
-if (number== 9) {
+if (number == 9)
+{
     Console.WriteLine("Неделя");
 }
-if ((number > 9)|(number < 1)) {
+if ((number > 9) | (number < 1))
+{
     Console.WriteLine("С таким числом дня недели нет");
 }
