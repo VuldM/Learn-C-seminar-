0e0b5c81206772f54cc97865542082339e5e7be3
VoldemarM <aniairina@gmail.com>
2023-04-08 15:53:40 +0300

    Build error Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -8,39 +8,40 @@ if (number == 1)
 {
     Console.WriteLine("Понедельник");
 }
-if (number == 2)
+else if (number == 2)
 {
     Console.WriteLine("Вторник");
 }
-if (number == 4)
+else if (number == 4)
 {
     Console.WriteLine("Четверг");
 }
-if (number == 3)
+else if (number == 3)
 {
     Console.WriteLine("Среда");
 }
-if (number == 5)
+else if (number == 5)
 {
     Console.WriteLine("Пятница");
 }
-if (number == 6)
+else if (number == 6)
 {
     Console.WriteLine("Шестица");
 }
-if (number == 7)
+else if (number == 7)
 {
     Console.WriteLine("Седьмица");
 }
-if (number == 8)
+else if (number == 8)
 {
     Console.WriteLine("Осьмица");
 }
-if (number == 9)
+else if (number == 9)
 {
     Console.WriteLine("Неделя");
 }
-if ((number > 9) | (number < 1))
+else
 {
     Console.WriteLine("С таким числом дня недели нет");
 }
+
