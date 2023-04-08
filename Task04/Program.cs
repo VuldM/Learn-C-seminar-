acdf410cfcb428d55e22a35c4f10c83f49b84d51
VoldemarM <aniairina@gmail.com>
2023-04-08 17:50:52 +0300

    added code in Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -3,3 +3,28 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ваши числа {numberA} {numberB} {numberC}");
+int max;
+if (numberA > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+if (max > numberC)
+{
+    Console.WriteLine($"Это число {max} больше всех");
+}
+else
+{
+    Console.WriteLine($"Это число {numberC} больше всех");
+}
