c61fe728848b873b789469fe02b3c9475634d0d5
VoldemarM <aniairina@gmail.com>
2023-04-08 16:39:22 +0300

    added folder Task07 and code

---
+++ Task07/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Напишите программу, которая принимает на вход
+// трехзначное число и на выходе показывает последнюю цифру этого числа.
+//  456 -> 6
+//  782 -> 2
+//  918 -> 8
+Console.WriteLine("Введите первое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = -number;
+}
+if (number >= 100 && number <= 999)
+{
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("Ваше число не трехзначное");
+}
