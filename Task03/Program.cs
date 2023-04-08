37730bb0fbef5aafad159bbec3ee47b93a791f6f
VoldemarM <aniairina@gmail.com>
2023-04-08 15:18:47 +0300

    change code into Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -2,3 +2,35 @@
 //  название дня недели по заданному номеру.
 //  3 -> Среда
 //  5-> Пятница
+Console.WriteLine("Введите число одного до девяти:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number== 1) {
+    Console.WriteLine("Понедельник");
+}
+if (number== 2) {
+    Console.WriteLine("Вторник");
+}
+if (number== 4) {
+    Console.WriteLine("Четверг");
+}
+if (number== 3) {
+    Console.WriteLine("Среда");
+}
+if (number== 5) {
+    Console.WriteLine("Пятница");
+}
+if (number== 6) {
+    Console.WriteLine("Шестица");
+}
+if (number== 7) {
+    Console.WriteLine("Седьмица");
+}
+if (number== 8) {
+    Console.WriteLine("Осьмица");
+}
+if (number== 9) {
+    Console.WriteLine("Неделя");
+}
+if (number > 9) {
+    Console.WriteLine("С таким числом дня недели нет");
+}
