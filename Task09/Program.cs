42a9ac0270e9eb28575043986a99eaf08b0b3b94
VoldemarM <aniairina@gmail.com>
2023-04-11 19:19:26 +0300

    change files

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,17 +5,35 @@
 // 12-> 2
 // 85 -> 8
 
-int number = new Random().Next(10, 100);// 99 + 1
-Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
-int firstDigit = number / 10;
-int secondDigit = number  % 10;
-if(firstDigit > secondDigit)
-{
-Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
-}
-{
-  Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");  
-}
+//             Обычный код
+// int number = new Random().Next(10, 100);// 99 + 1
+// Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+// int firstDigit = number / 10;
+// int secondDigit = number  % 10;
 
+// if(firstDigit > secondDigit)
+// {
+// Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// }
+// else
+// {
+//   Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");  
+// }
+
+// int maxDigit = firstDigit>secondDigit ? firstDigit : secondDigit;
+
+//  Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int number = new Random().Next(10, 100);   //  Генератор случайого числа от 10 до 99
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}"); // Вывод этого числв терминал
+int maxDigit = MaxDigit(number); // Присваивание значения метода(функции) MaxDigit переменной maxDigit
+ Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}"); // ВЫвод значения в теримнал
+
+//            С использование Методов(функций)
+ int MaxDigit (int num) // метод MaxDigit возвращает наибольшую цифру в числе 
+{ // тело метода
+int firstDigit = num / 10;
+int secondDigit = num  % 10;
 int maxDigit = firstDigit>secondDigit ? firstDigit : secondDigit;
- Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+return maxDigit;
+ } // тело метода
