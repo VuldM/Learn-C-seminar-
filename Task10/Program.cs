4a15525ceb4b8760ee3f9529ff9d8c616a7c49af
VoldemarM <aniairina@gmail.com>
2023-04-12 21:05:26 +0300

    added code in folder Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,3 +3,17 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number < 100 || number > 999)
+{
+    Console.WriteLine("Введите пожалуйста трехзначное число:");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine ("Второе число " + MiddleDigit(number));
+
+int MiddleDigit (int num)
+{
+int Digit3 = num / 10;
+return Digit3 % 10;
+}
