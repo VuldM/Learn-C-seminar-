1425cd9c60a66a75f959a876d4662f9371b2971a
VoldemarM <aniairina@gmail.com>
2023-04-12 22:05:44 +0300

    added code in folder Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,3 +2,17 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+while (number < 99 || number >  1000)
+{
+    Console.WriteLine("Работает");
+    number = number / 10;
+}
+    Console.WriteLine(number);
+Console.WriteLine($" - > {number % 10}");
