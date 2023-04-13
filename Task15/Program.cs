2c1a5c0796f45bb68cd782030a22fcfe1d6fe344
VoldemarM <aniairina@gmail.com>
2023-04-13 11:01:24 +0300

    added code homework assignments the folder Task 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -2,3 +2,20 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.WriteLine("Введите число дня недели: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+if (dayWeek < 1 || dayWeek > 7)
+{
+    Console.WriteLine("Вы вышли за диапазн числового значения дня недели");
+}
+
+else
+{
+bool res = WeekEnd(dayWeek); //булева операция да, нет
+Console.WriteLine(res  ? "да" : "нет"); 
+}
+
+bool WeekEnd(int num)
+{
+   return num == 6 || num == 7; 
+}
