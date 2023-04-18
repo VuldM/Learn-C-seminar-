fc30f137fbdc0b0098586e5d9a4eb97f6d8d5915
VuldM <aniairina@gmail.com>
2023-04-18 21:18:20 +0300

    write code in Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,3 +3,38 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray(int sze)//  Если массив создается в методе то его надо вернуть оператором ретурн
+{
+    int[] arr = new int[sze];
+    Random rnd = new Random(); //var rnd = new Random()ж или так
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arra) // Если в методе ничего не создается, то можно использовать войд метод
+{
+    for (int i = 0; i < arra.Length; i++)
+    {
+      if(i < arra.Length - 1)  Console.Write($"{arra[i]}, ");  
+    if(i == arra.Length - 1)  Console.Write($"{arra[i]} ");      
+   
+    }           
+    
+     Console.Write(" -> ["); 
+    for (int i = 0; i < arra.Length; i++)
+    {
+         if(i < arra.Length - 1)  Console.Write($"{arra[i]}, ");  
+         if(i == arra.Length - 1)  Console.Write($"{arra[i]}"); 
+    }
+     Console.Write($"]"); 
+
+}
+
+Console.WriteLine("Please enter array length: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
