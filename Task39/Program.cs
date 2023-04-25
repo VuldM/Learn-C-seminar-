4614bdd481a12274b7a748d253de3d001957d99c
VoldemarM <aniairina@gmail.com>
2023-04-25 14:56:55 +0300

    added folder task42

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+int[] CreateArray(int sze)//  Если массив создается в методе то его надо вернуть оператором ретурн
+{
+    int[] arr = new int[sze];
+    Random rnd = new Random(); //var rnd = new Random()ж или так
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arra) // Если в методе ничего не создается, то можно использовать войд метод
+{
+    int i = 0;
+    for (i = 0; i < arra.Length - 1; i++)
+    {
+        Console.Write($"{arra[i]}, ");
+
+    }
+    Console.Write($"{arra[i]} ");
+    Console.Write(" -> [");
+    for (i = 0; i < arra.Length - 1; i++)
+    {
+        Console.Write($"{arra[i]}, ");
+
+    }
+    Console.Write($"{arra[i]}");
+    Console.WriteLine($"]");
+}
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1];
+        arr[arr.Length - 1 - i] = temp;
+
+    }
+}
+
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
--- Task40/Program.cs
+++ Task40/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Please enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool triangleSearch = TriangleSearch(number1, number2, number3);
+Console.WriteLine(triangleSearch ? "такой треугольник существует" : "такой треугольник не существует");
+
+bool TriangleSearch(int num1, int num2, int num3)
+{
+    return num1 + num2 > num3 && num2 + num3 > num1 && num1 + num3 > num2;
+}
+
