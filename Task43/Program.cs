9c8062b05ad5333bf40c664b6bbcba82561127d6
VuldM <aniairina@gmail.com>
2023-04-25 22:06:28 +0300

    changed code on the folder Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -11,16 +11,18 @@ int numberB2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("K2: ");
 int numberK2 = Convert.ToInt32(Console.ReadLine());
 
-double PointX(int b1, int k1, int b2, int k2)
+double PointX(double b1, double k1, double b2, double k2)
 {
   
     double x = (b2 - b1) / (k1 - k2);
+
+ //   Console.WriteLine($"X: {x} ");
 // k1*x + b1 - y = k2 + x + b2 - y; k1*x - k2*x + b1 - y = b2 - y; k1*x - k2*x + b1 = b2 - y + y; k1*x - k2 * x = b2 - y + y - b1; k1*x - k2*x = b2 - b1
 //x = b2 - b1 / k 1 - k 2
 return x;
 }
  
-double PointY(int b1, int k1, int b2, int k2)
+double PointY(double b1, double k1, double b2, double k2)
 {
       double x = (b2 - b1) / (k1 - k2);
     double y = k1 * x + b1;
@@ -30,4 +32,4 @@ return y;
 }
 double x = PointX(numberB1, numberK1, numberB2, numberK2);
 double y = PointY(numberB1, numberK1, numberB2, numberK2);
-Console.WriteLine($"{x} -> {y}");
+Console.WriteLine($"b1 = {numberB1}, k1 = {numberK1}, b2 = {numberB2}, k2 = {numberK2} -> ({x}; {y})");
